2ee3be1639ab9bec10465c61d7bf61ef912b284e
Chee Feng Cheong <cheefengcheong@gmail.com>
2022-11-23 01:21:59 +0800

    Add design for Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../images/logo.png";
+import {
+  Home,
+  ExploreOutlined,
+  SubscriptionsOutlined,
+  VideoCallOutlined,
+  HistoryOutlined,
+  LibraryMusicOutlined,
+  SportsEsportsOutlined,
+  SportsBasketballOutlined,
+  MovieOutlined,
+  ArticleOutlined,
+  LiveTvOutlined,
+  AccountCircleOutlined,
+  SettingsOutlined,
+  FlagOutlined,
+  HelpOutlineOutlined,
+  SettingsBrightnessOutlined,
+} from "@mui/icons-material";
+const Container = styled.div`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+`;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0px;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid #373737;
+`;
+
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+function Menu() {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={logo} />
+          YouTube
+        </Logo>
+        <Item>
+          <Home />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlined />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlined />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoCallOutlined />
+          Video
+        </Item>
+        <Item>
+          <HistoryOutlined />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to Like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlined />
+            SIGN IN
+          </Button>
+        </Login>
+        <Hr />
+        <Item>
+          <LibraryMusicOutlined />
+          Music
+        </Item>
+        <Item>
+          <SportsEsportsOutlined />
+          Gaming
+        </Item>
+        <Item>
+          <SportsBasketballOutlined />
+          Sports
+        </Item>
+        <Item>
+          <MovieOutlined />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlined />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlined />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <AccountCircleOutlined />
+          Account
+        </Item>
+        <Item>
+          <SettingsOutlined />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlined />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlined />
+          Help
+        </Item>
+        <Item>
+          <SettingsBrightnessOutlined />
+          Light Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Menu;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      Navbar<div>test</div>
+    </div>
+  );
+}
+
+export default Navbar;
