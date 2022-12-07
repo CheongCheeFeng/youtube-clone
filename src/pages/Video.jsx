5f80eb58a81ae6d49729e597b5505e8ced56257a
Chee Feng Cheong <cheefengcheong@gmail.com>
2022-12-08 01:14:28 +0800

    Design Video page

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,7 +1,166 @@
+import {
+  AddTaskOutlined,
+  ReplyOutlined,
+  ThumbDownOffAltOutlined,
+  ThumbUpOutlined,
+} from "@mui/icons-material";
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Recomendation = styled.div`
+  flex: 2;
+`;
+
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ChannelInfo = styled.div`
+  gap: 20px;
+  display: flex;
+`;
+
+const ChannelDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.div`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.div`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`;
+
+const Description = styled.p`
+  font-size: 14px;
+`;
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #999;
+`;
+
+const Subsribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
 
 const Video = () => {
-  return <div>Video</div>;
+  return (
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Video Title</Title>
+        <Details>
+          <Info>1.2M views • 2 days ago</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlined /> 123
+            </Button>
+            <Button>
+              <ThumbDownOffAltOutlined /> Disliked
+            </Button>
+            <Button>
+              <ReplyOutlined /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlined /> Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"></Image>
+            <ChannelDetails>
+              <ChannelName>My Channel</ChannelName>
+              <ChannelCounter>200K subscribers</ChannelCounter>
+              <Description>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+                autem nobis quos fugit quisquam, voluptate odio quia ut dolore
+                illo, accusantium dolorum sed, reprehenderit voluptatibus? Unde
+                dolores soluta delectus eius.
+              </Description>
+            </ChannelDetails>
+          </ChannelInfo>
+          <Subsribe>Subscribe</Subsribe>
+        </Channel>
+      </Content>
+      <Recomendation>recomendation</Recomendation>
+    </Container>
+  );
 };
 
 export default Video;
