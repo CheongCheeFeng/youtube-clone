de1a47aead48d9057b15af89a138f1f56f8707d8
Chee Feng Cheong <cheefengcheong@gmail.com>
2023-01-05 22:34:14 +0800

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
 const PORT = 8800;
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on port ${PORT}`);
+  console.log("Connected to Server");
 });
