bbbad197a3a50e9d1d3ceb53d39029f4cd6554e6
Chee Feng Cheong <cheefengcheong@gmail.com>
2023-01-06 00:00:00 +0800

    Create Rest API

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
 
+mongoose.set("strictQuery", false);
 const PORT = 8800;
 const app = express();
 dotenv.config();
@@ -13,12 +17,16 @@ const connect = () => {
       console.log("Connected to MongoDB");
     })
     .catch((err) => {
+      console.log("Error connecting to MongoDB");
       throw err;
     });
 };
 
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
 app.listen(PORT, () => {
   connect();
   console.log(`Server is running on port ${PORT}`);
-  console.log("Connected to Server");
 });
