e855bb4780c38a36fd84d3c6d5b5dfddab98f55d
Chee Feng Cheong <cheefengcheong@gmail.com>
2023-01-12 00:37:19 +0800

    Create api for update delete and get user

---
+++ server/controllers/user.js
@@ -0,0 +1,50 @@
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You are not allowed to update this user!"));
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(createError(403, "You are not allowed to delete this user!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const subscribe = async (req, res, next) => {};
+
+export const unsubscribe = async (req, res, next) => {};
+
+export const likeVideo = async (req, res, next) => {};
+
+export const dislikeVideo = async (req, res, next) => {};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,36 @@
 import express from "express";
-import {} from "../controllers/user.js";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  likeVideo,
+  dislikeVideo,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+// update user
+router.put("/:id", verifyToken, updateUser);
+
+// delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+// get user
+router.get("/find/:id", getUser);
+
+// subscribe a user
+router.put("/subscribe/:id", verifyToken, subscribe);
+
+// unsubscribe a user
+router.put("/unsubscribe/:id", verifyToken, unsubscribe);
+
+// like a video
+router.put("/like/:videoId", verifyToken, likeVideo);
+
+// dislike a video
+router.put("/dislike/:videoId", verifyToken, dislikeVideo);
 
 export default router;
