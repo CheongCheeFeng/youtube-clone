520f2aa940252b7190f1936be843ede9d26d0a61
Chee Feng Cheong <cheefengcheong@gmail.com>
2023-04-08 01:08:29 +0800

    Implement add, delete and get comments api

---
+++ server/controllers/comment.js
@@ -0,0 +1,41 @@
+import Video from "../models/Video.js";
+import Comment from "../models/Comment.js";
+import { createError } from "../error.js";
+
+export const addComment = async (req, res, next) => {
+  try {
+    const newComment = new Comment({
+      ...req.body,
+      userId: req.user.id,
+    });
+    const savedComment = await newComment.save();
+    res.status(200).send(savedComment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(comment.videoId);
+    if (!comment) return next(createError(404, "Comment not found!"));
+    if (comment.userId === req.user.id || video.userId === req.user.id) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("Comment has been deleted...");
+    } else {
+      next(createError(403, "You can only delete your comment!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ videoId: req.params.videoId });
+    res.status(200).json(comments);
+  } catch (err) {
+    next(err);
+  }
+};
