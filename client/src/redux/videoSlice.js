fc2e796d7672b8c4bf4c72686969e7cff891e66a
Chee Feng Cheong <cheefengcheong@gmail.com>
2023-04-16 02:46:20 +0800

    Presist user state

---
+++ client/src/redux/videoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentVideo: null,
+  loading: false,
+  error: false,
+};
+
+export const videoSlice = createSlice({
+  name: "video",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.currentVideo = action.payload;
+      state.loading = false;
+    },
+    loginFailure: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+    logout: (state) => {
+      return initialState;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  videoSlice.actions;
+
+export default videoSlice.reducer;
